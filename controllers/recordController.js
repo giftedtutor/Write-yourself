a4ebc12c894fa0464094dd7892a766e3ab3297c8
imhky <giftedtutorr@gmail.com>
2022-10-30 23:57:15 +0500

    getRecords and updateRecord APIs done

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -9,10 +9,42 @@ const addRecord = asyncHandler(async (req, res) => {
   });
   try {
     const record = await recordData.save();
-    res.status(200).json({Message: 'Record Added Successfully'});
+    res.status(200).json({ Message: "Record Added Successfully" });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-export { addRecord };
+// get record data daily base
+
+const getRecords = asyncHandler(async (req, res) => {
+  try {
+    const record = await Record.find({
+      user_id: req.query.user_id,
+    });
+    res.status(200).json(record);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// (Submit) Update data of each record
+
+const updateRecord = asyncHandler(async (req, res) => {
+  const recordData = {
+    _id: req.body._id,
+    user_id: req.body.user_id,
+    list: req.body.list,
+  };
+  try {
+    const fData = await Record.findOneAndUpdate(
+      { _id: req.body._id },
+      recordData
+    );
+    res.status(200).json({ message: "Record Updated" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export { addRecord, getRecords, updateRecord};
--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import {
   addRecord,
+  getRecords,
+  updateRecord,
 } from "../controllers/recordController.js";
 const router = express.Router();
 
 // record table routes
 
 router.post("/addRecord", addRecord);
+router.get("/getRecords", getRecords);
+router.put("/updateRecord", updateRecord);
 
 export default router;
